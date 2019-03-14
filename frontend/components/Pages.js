ce6c07748df34089b0d629ee93a9647ff90a99e0
JasonChang <jason@ai-pattern.com>
2019-03-14 16:37:44 +0800

    front Meta

--- frontend/components/Pages.js
+++ frontend/components/Pages.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Meta from '../components/Meta';
 
 class Pages extends Component {
   render() {
     return (
       <div>
+        <Meta></Meta>
         <Header></Header>
         {this.props.children}
       </div>
