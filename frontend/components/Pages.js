dc574d743c0156aac0d2b9e50f8ad7221912fdd5
JasonChang <jason@ai-pattern.com>
2019-03-14 17:31:41 +0800

    styled components globel veriable by themeProvider

--- frontend/components/Pages.js
+++ frontend/components/Pages.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
-import styled from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
-const MyButton = styled.button`
-  background-color: red;
-  font-size: ${props => props.huge ? '100px' : '20px'};
-  .poop {
-    font-size: 25px;
-  }
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offwhite: '#EDEDED',
+  maxWidth: '1000px',
+  boxShadow: '0 12px 24px 0 rgba(0, 0, 0, 0.09)'
+};
+
+const StylePage = styled.div`
+  background-color: white;
+  color: black;
+`;
+const Inner = styled.div`
+  max-width: ${ props => props.theme.maxWidth };
+  margin: 0 auto;
+  padding: 2rem;
+  border: 1px solid blue;
 `;
 
 class Pages extends Component {
   render() {
     return (
-      <div>
-        <Meta></Meta>
-        <Header></Header>
-        <MyButton huge>click me!<span className="poop">💩</span></MyButton>
-        <MyButton>click me!<span className="poop">💩</span></MyButton>
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StylePage>
+          <Meta></Meta>
+          <Header></Header>
+          <Inner>
+            {this.props.children}
+          </Inner>
+        </StylePage>
+      </ThemeProvider>
     )
   }
 }
