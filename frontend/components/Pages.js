9ce8e0e6986c4a0b8fc14dd11b9473cd51b21a8c
JasonChang <jason@ai-pattern.com>
2019-03-14 18:27:33 +0800

    front global styling and Typography with Styled Components

--- frontend/components/Pages.js
+++ frontend/components/Pages.js
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import Meta from '../components/Meta';
 import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
+// 全域CSS變數 用ThemeProvider 倒進 各個styled.component 的props中，
 const theme = {
   red: '#FF0000',
   black: '#393939',
@@ -12,6 +13,34 @@ const theme = {
   maxWidth: '1000px',
   boxShadow: '0 12px 24px 0 rgba(0, 0, 0, 0.09)'
 };
+
+// css normalize
+injectGlobal`
+  @font-face {
+    font-family: 'radnika-next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika-next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
 
 const StylePage = styled.div`
   background-color: white;
