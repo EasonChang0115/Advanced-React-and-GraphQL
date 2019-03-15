06c56b0ac3a9187a4c0e6e2bfdfef45d31517b1f
JasonChang <jason@ai-pattern.com>
2019-03-15 15:07:50 +0800

    開啟server

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,22 +1,21 @@
 const { GraphQLServer } = require('graphql-yoga');
-
 const Mutation = require('./resolvers/Mutation');
 const Query = require('./resolvers/Query');
 const db = require('./db');
 
-// Create the GraphQL Yogo Server
+// Create the GraphQL Yoga Server
 
 function createServer() {
   return new GraphQLServer({
     typeDefs: 'src/schema.graphql',
     resolvers: {
       Mutation,
-      Query
+      Query,
     },
     resolverValidationOptions: {
-      requireResolversForResolveType: false
+      requireResolversForResolveType: false,
     },
-    context: req => ({...req, db})
+    context: req => ({ ...req, db }),
   });
 }
 
--- backend/src/db.js
+++ backend/src/db.js
@@ -2,10 +2,10 @@
 const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
-  typeDefs: './generated/prisma.graphql',
+  typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret:　process.env.PRISMA_SECRET,
-  debug: false // 若開啟 會在每次發出請求時 console出來內容
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
 });
 
 module.exports = db;
