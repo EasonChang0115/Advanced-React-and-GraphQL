3cc12b65e39ff8550e18e302f21469d86f91551c
JasonChang <jason@ai-pattern.com>
2019-03-15 14:49:08 +0800

    在index引入並start server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,16 @@
 // let's go!
+require('dotenv').config({ path: 'variable.env' });
+const createServer = require('createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO use express middleware to handle cookies (JWT)
+// TODO use express middleware to populate current user
+
+server.start({
+  cors: {
+    credentail: true,
+    origin: proscess.env.FRONTEND_URL
+  }
+}, (deets) => {console.log(`server started on port: http:/localhost:${deets.port}`);});
