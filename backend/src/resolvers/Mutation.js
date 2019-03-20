35f3949bcdd18df4f513b6a56e2bfdf59da47f84
JasonChang <jason@ai-pattern.com>
2019-03-20 11:46:25 +0800

    更新Item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -9,10 +9,20 @@ const Mutations = {
       },
       info
     );
-
-    console.log(item);
-
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the udates
+    const updates = {...args};
+    // remove the ID from updates
+    delete updates.id;
+    // run update method
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where : {
+        id: args.id
+      },
+    }, info);
   }
 };
 
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,8 @@
 const { forwardTo } = require('prisma-binding');
 const Query = {
   // 若不需要驗證 prisma-binding 提供歷遍db的功能 可以直接取出要的資料
-  items: forwardTo('db')
+  items: forwardTo('db'),
+  item: forwardTo('db')
   // async items(parent, args, ctx, info) {
   //   console.log('Getting Items');
   //   const items = await ctx.db.query.items();
