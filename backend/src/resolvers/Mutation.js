31ea65b3d1443b6151b2f5420ca8f8126dfe7a6b
JasonChang0115 <jasonchang@panmedia.asia>
2019-03-21 01:39:45 +0800

    刪除功能

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -23,6 +23,15 @@ const Mutations = {
         id: args.id
       },
     }, info);
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{
+      id title }`);
+    // 2. Check if they own that item, on have the permissioins
+    // 3. Delete it!
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
