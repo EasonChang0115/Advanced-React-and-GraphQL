b0330f338ff4120e9131fb9bf23f2a073d2b064b
JasonChang0115 <jasonchang@panmedia.asia>
2019-03-22 00:39:14 +0800

    後端新增使用者 產生JWT 存入cookie 和 權限(permission)

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are loggef in
@@ -32,6 +35,29 @@ const Mutations = {
     // 2. Check if they own that item, on have the permissioins
     // 3. Delete it!
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    //  create the user in the database
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        password,
+        permission: { set: ['USER'] }
+      }
+    }, info);
+    // create the JWT for them
+    const token = jwt.sign({ userId: user.Id }, process.env.APP_SECRET);
+    // we set the jwt as a cookie on the reponse
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 20 * 365 // 1 year cookie
+    });
+    // finally we return the user to the brower
+    return user;
   }
 };
 
