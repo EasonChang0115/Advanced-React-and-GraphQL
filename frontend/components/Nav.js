c9dc798d274a1a2b8c50015acb66a804b2720ce0
JasonChang <jason@ai-pattern.com>
2019-03-25 11:31:52 +0800

    若有登入 顯示登入人姓名於Nav bar (childern pass function)

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,7 +1,14 @@
 import Link from 'next/link';
-import NavStyles from './styles/NavStyles'
+import NavStyles from './styles/NavStyles';
+import User from './User';
 const Nav = () => (
 	<NavStyles>
+    <User>
+      {({data: { me }}) => {
+        if (me) return <a>{me.name}</a>;
+        return null;
+      }}
+    </User>
     <Link href="/items">
 			<a>Shop</a>
 		</Link>
